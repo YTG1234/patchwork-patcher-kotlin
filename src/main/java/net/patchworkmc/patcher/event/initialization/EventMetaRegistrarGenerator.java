5a58ec8d02219baa4e6119a7b657efb51742b577
sschr15 <steven.sschr15@hotmail.com>
2021-01-03 16:36:02 -0600

    Old sided EventBusSubscriber setup

--- src/main/java/net/patchworkmc/patcher/event/initialization/EventMetaRegistrarGenerator.java
+++ src/main/java/net/patchworkmc/patcher/event/initialization/EventMetaRegistrarGenerator.java
@@ -1,12 +1,12 @@
 package net.patchworkmc.patcher.event.initialization;
 
+import net.patchworkmc.patcher.Patchwork;
 import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 
-import net.patchworkmc.patcher.Patchwork;
 import net.patchworkmc.patcher.event.EventBusSubscriber;
 import net.patchworkmc.patcher.event.EventConstants;
 import net.patchworkmc.patcher.event.SubscribingClass;
@@ -23,6 +23,27 @@ public final class EventMetaRegistrarGenerator {
 		// We store the instance as a local variable for more readable decompiled code.
 		method.visitFieldInsn(Opcodes.GETSTATIC, EVENT_REGISTRAR_REGISTRY, "INSTANCE", EVENT_REGISTRAR_REGISTRY_INSTANCE_SIGNATURE);
 		method.visitVarInsn(Opcodes.ASTORE, 1);
+
+		{
+			Label type = new Label();
+			Label end = new Label();
+			// FabricLoader.getInstance().getEnvironmentType()
+			method.visitMethodInsn(Opcodes.INVOKESTATIC, "net/fabricmc/loader/api/FabricLoader", "getInstance", "()Lnet/fabricmc/loader/api/FabricLoader;", true);
+			method.visitMethodInsn(Opcodes.INVOKEINTERFACE, "net/fabricmc/loader/api/FabricLoader", "getEnvironmentType", "()Lnet/fabricmc/api/EnvType;", true);
+
+			// EnvType.CLIENT
+			method.visitFieldInsn(Opcodes.GETSTATIC, "net/fabricmc/api/EnvType", "CLIENT", "Lnet/fabricmc/api/EnvType;");
+
+			method.visitJumpInsn(Opcodes.IF_ACMPNE, type);
+			method.visitInsn(Opcodes.ICONST_1);
+			method.visitJumpInsn(Opcodes.GOTO, end);
+
+			method.visitLabel(type);
+			method.visitInsn(Opcodes.ICONST_0);
+
+			method.visitLabel(end);
+			method.visitVarInsn(Opcodes.ISTORE, 2);
+		}
 
 		String className = subscriber.getClassName();
 
@@ -46,9 +67,14 @@ public final class EventMetaRegistrarGenerator {
 		if (annotation != null) {
 			// TODO: Check targetModId
 
+			Label skip = new Label();
+
 			if (!annotation.isClient() || !annotation.isServer()) {
-				Patchwork.LOGGER.warn("Sided @EventBusSubscriber annotations are not supported yet, applying {} from {} : {} without sides.",
+				Patchwork.LOGGER.warn("Sided @EventBusSubscriber annotations are still in progress, applying {} from {} : {}",
 						annotation, subscriber.getClassName(), annotation.getTargetModId());
+
+				if (annotation.isClient()) checkSide(true, method, skip);
+				if (annotation.isServer()) checkSide(false, method, skip);
 			}
 
 			if (annotation.getBus() == EventBusSubscriber.Bus.MOD) {
@@ -62,12 +88,20 @@ public final class EventMetaRegistrarGenerator {
 			method.visitLdcInsn(Type.getObjectType(subscriber.getClassName()));
 
 			method.visitMethodInsn(Opcodes.INVOKEINTERFACE, "net/minecraftforge/eventbus/api/IEventBus", "register", "(Ljava/lang/Object;)V", true);
+
+			if (!annotation.isClient() || !annotation.isServer()) method.visitLabel(skip);
 		}
 
 		method.visitInsn(Opcodes.RETURN);
 		method.visitLocalVariable("registryInstance", EVENT_REGISTRAR_REGISTRY_INSTANCE_SIGNATURE, null, new Label(), new Label(), 1);
-		method.visitMaxs(3, 2);
+		method.visitLocalVariable("isClient", "Z", null, new Label(), new Label(), 2);
+		method.visitMaxs(3, 3);
 		method.visitEnd();
+	}
+
+	private static void checkSide(boolean isClient, MethodVisitor method, Label skip) {
+		method.visitVarInsn(Opcodes.ILOAD, 2);
+		method.visitJumpInsn(isClient ? Opcodes.IFEQ : Opcodes.IFNE, skip);
 	}
 
 	private EventMetaRegistrarGenerator() {
