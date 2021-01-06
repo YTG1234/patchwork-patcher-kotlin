55ac295b3c7a4d62954732a76dbaac1460e83de7
YTG123 <54103478+YTG1234@users.noreply.github.com>
2021-01-06 16:02:44 +0200

    removed stuff

--- src/main/kotlin/net/patchworkmc/patcher/ForgeModJar.kt
+++ src/main/kotlin/net/patchworkmc/patcher/ForgeModJar.kt
@@ -7,8 +7,7 @@ import net.patchworkmc.manifest.mod.ModManifest
 import net.patchworkmc.patcher.annotation.AnnotationStorage
 import java.nio.file.Path
 
-// TODO: Remove @JvmOverloads
-open class ForgeModJar @JvmOverloads constructor(val inputPath: Path, val outputPath: Path, val manifest: ModManifest, val accessTransformer: ForgeAccessTransformer? = null) {
+open class ForgeModJar(val inputPath: Path, val outputPath: Path, val manifest: ModManifest, val accessTransformer: ForgeAccessTransformer? = null) {
     val annotationStorage: AnnotationStorage = AnnotationStorage()
     private val entrypoints: JsonObject = JsonObject()
     var isProcessed = false
